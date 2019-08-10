487c6c1956f77e48395b3df34aad456b7bb339db
gabriel silva <gasil96@gmail.com>
2019-08-10 20:59:31 -0300

    projeto convertido para maven spring adicionado jpa adicionado pom.xml com paramatros passados

---
+++ src/main/java/br/com/gbsoftware/spacetattoostudio/SpacetattooApplication.java
@@ -0,0 +1,14 @@
+package br.com.gbsoftware.spacetattoostudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpacetattooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpacetattooApplication.class, args);
+		
+	}
+	
+}
