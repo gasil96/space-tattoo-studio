35e66095567755dc7293bd51641111c1689a9d03
gabriel silva <gasil96@gmail.com>
2019-08-23 21:38:27 -0300

    classe configuration security criada

---
+++ src/main/java/br/com/gbsoftware/spacetattoostudio/config/WebSecurityConfig.java
@@ -0,0 +1,13 @@
+package br.com.gbsoftware.spacetattoostudio.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+
+	
+	
+}
