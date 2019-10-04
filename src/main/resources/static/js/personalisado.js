24af534889fc7f713c18366c5170f3d77c29b3b1
gabriel silva <gasil96@gmail.com>
2019-10-04 03:35:10 -0300

    refin cores paleta
    Signed-off-by: gabriel silva <gasil96@gmail.com>

--- src/main/resources/static/js/personalisado.js
+++ src/main/resources/static/js/personalisado.js
@@ -1,6 +1,6 @@
 // TROCAR DE BRANCO PRA VERDE NA PASSADA DO CURSOR
 function iconOnOver(x) {
-	x.style.color = "green";
+	x.style.color = "#429244";
 }
 function iconNormal(x) {
 	x.style.color = "white";
