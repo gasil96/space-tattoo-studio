4d2834f22be67604b48909462268ece1dca0144d
gabriel silva <gasil96@gmail.com>
2019-10-09 18:19:45 -0300

    finalzando parte caixa;
    Signed-off-by: gabriel silva <gasil96@gmail.com>

--- src/main/resources/static/js/personalisado.js
+++ src/main/resources/static/js/personalisado.js
@@ -14,45 +14,63 @@ function iconNormalPersonalizado(x) {
 	x.style.color = "#bb414d";
 }
 
+$(document).ready(
+		function() {
+			$.ajax({
+				type : "GET",
+				url : "/caixa/input-clientes",
+				dataType : 'json',
+				success : function(item) {
+					var jsonOk = JSON.stringify(item);
+					$('#typeaheadIdInstaNomeCliente').typeahead(
+							{
+
+								displayText : function(item) {
+									return item.nome + ' , ' + item.telefone
+											+ ' , ' + item.instagram
+								},
+								afterSelect : function(item) {
+									this.$element[0].value = item.id
+								},
+								source : item
+							});
+
+					$('#typeaheadIdInstaNomeCliente2').typeahead(
+							{
+
+								displayText : function(item) {
+									return item.nome + ' , ' + item.telefone
+											+ ' , ' + item.instagram
+								},
+								afterSelect : function(item) {
+									this.$element[0].value = item.id
+								},
+								source : item
+							});
+					$('#consultaCadastroCliente').typeahead(
+							{
+
+								displayText : function(item) {
+									return item.nome + ', ' + item.telefone
+											+ ', ' + item.instagram
+								},
+								afterSelect : function(item) {
+									this.$element[0].value = item.nome + ', '
+											+ item.telefone + ', '
+											+ item.instagram
+								},
+								source : item
+							});
+				},
+				error : function(e) {
+					alert("Ocorreu um erro, contate o suporte!");
+				}
+			});
+
+		});
 
 $(document).ready(function() {
-	$.ajax({
-		type : "GET",
-		url : "/caixa/input-clientes",
-		dataType : 'json',
-		success : function(item) {
-			var jsonOk = JSON.stringify(item);
-			$('#typeaheadIdInstaNomeCliente').typeahead({
-				
-			    displayText: function (item) {
-			        return item.nome + ' , ' + item.telefone + ' , ' + item.instagram
-			    },
-			    afterSelect: function (item) {
-			        this.$element[0].value = item.id
-			    },
-			    source: item
-			});
-				
-			$('#consultaCadastroCliente').typeahead({
-				
-			    displayText: function (item) {
-			        return item.nome + ', ' + item.telefone + ', ' + item.instagram
-			    },
-			    afterSelect: function (item) {
-			        this.$element[0].value = item.nome + ', ' + item.telefone + ', ' + item.instagram
-			    },
-			    source: item
-			});
-		},
-		error : function(e) {
-			alert("Ocorreu um erro, contate o suporte!");
-		}
+	$('[data-toggle="popover"]').popover({
+		trigger : 'hover'
 	});
-
 });
-
-$(document).ready(function(){
-	  $('[data-toggle="popover"]').popover({
-		  trigger: 'hover'
-	  });   
-	});
