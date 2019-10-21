4d37e1ab46d04cae7d571a334ae97c0ffe1df6d5
Gabriel Silva <gasil96@gmail.com>
2019-10-21 13:52:45 -0300

    faltando implementar gerar relatorio...
    Signed-off-by: Gabriel Silva <gasil96@gmail.com>

--- src/main/java/br/com/gbsoftware/spacetattoostudio/controller/FinanceiroController.java
+++ src/main/java/br/com/gbsoftware/spacetattoostudio/controller/FinanceiroController.java
@@ -1,6 +1,10 @@
 package br.com.gbsoftware.spacetattoostudio.controller;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.List;
+
+import javax.xml.ws.Response;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,6 +12,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.FontFactory;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
 
 import br.com.gbsoftware.spacetattoostudio.domain.model.Caixa;
 import br.com.gbsoftware.spacetattoostudio.service.CaixaService;
@@ -57,12 +67,41 @@ public class FinanceiroController {
 		List<Caixa> listaRelatorioGeralMensal = servicoCaixa.buscarTodosMes(relGeralMensal);
 
 		if (!listaRelatorioGeralMensal.isEmpty()) {
-			List<Object> relatorio =  servicoCaixa.relatorio(relGeralMensal);
-						model.addAttribute("totalGeral", relatorio.get(0));
-						model.addAttribute("totalCredito", relatorio.get(1));
-						model.addAttribute("totalDebito", relatorio.get(2));
-						model.addAttribute("totalAvista", relatorio.get(3));
-			
+			List<Object> relatorio = servicoCaixa.relatorio(relGeralMensal);
+			model.addAttribute("totalGeral", relatorio.get(0));
+			model.addAttribute("totalCredito", relatorio.get(1));
+			model.addAttribute("totalDebito", relatorio.get(2));
+			model.addAttribute("totalAvista", relatorio.get(3));
+
+			Document relatorioPDF = new Document();
+			try {
+
+//				PdfWriter.getInstance(relatorioPDF,
+//						new FileOutputStream("Relatorio_Mensal_Space_Tattoo_Studio.pdf"));
+				PdfWriter.getInstance(relatorioPDF, new FileOutputStream(relGeralMensal));
+				relatorioPDF.open();
+
+				// adicionando um parágrafo ao documento
+				relatorioPDF.add(new Paragraph("Relatório Mensal (nomeDoMes)"));
+
+				// adicionando um parágrafo com fonte diferente ao arquivo
+				relatorioPDF.add(new Paragraph("Total Geral: R$ " + relatorio.get(0),
+						FontFactory.getFont(FontFactory.COURIER, 12)));
+				relatorioPDF.add(new Paragraph("Total Arrecadado no Crédito: R$ " + relatorio.get(1),
+						FontFactory.getFont(FontFactory.COURIER, 12)));
+				relatorioPDF.add(new Paragraph("Total Arrecadado no Débito: R$ " + relatorio.get(2),
+						FontFactory.getFont(FontFactory.COURIER, 12)));
+				relatorioPDF.add(new Paragraph("Total Arrecadado À Vista: R$ " + relatorio.get(3),
+						FontFactory.getFont(FontFactory.COURIER, 12)));
+
+			} catch (DocumentException de) {
+				System.err.println(de.getMessage());
+			} catch (IOException ioe) {
+				System.err.println(ioe.getMessage());
+			} finally {
+				relatorioPDF.close();
+			}
+
 			return MODAL_DETALHAMENTO_MENSAL;
 
 		} else {
@@ -72,4 +111,46 @@ public class FinanceiroController {
 
 	}
 
+//	// TODO - MÉTODO EM ANDAMENTO
+//	@RequestMapping("gerar-pdf")
+//	public String gerarPdf(@RequestParam(value = "relGeralMensal", required = true) String relGeralMensal,
+//			Model model) {
+//		List<Caixa> listaRelatorioGeralMensal = servicoCaixa.buscarTodosMes(relGeralMensal);
+//
+//		if (!listaRelatorioGeralMensal.isEmpty()) {
+//			List<Object> relatorio = servicoCaixa.relatorio(relGeralMensal);
+//			Document relatorioPDF = new Document();
+//			try {
+//
+//				PdfWriter.getInstance(relatorioPDF,
+//						new FileOutputStream("C:\\Users\\14882\\Downloads\\pdf_ReceitasDeCodigo.pdf"));
+//				relatorioPDF.open();
+//
+//				// adicionando um parágrafo ao documento
+//				relatorioPDF.add(new Paragraph("Relatório Mensal (nomeDoMes)"));
+//
+//				// adicionando um parágrafo com fonte diferente ao arquivo
+//				relatorioPDF.add(new Paragraph("Total Geral: R$ " + relatorio.get(0),
+//						FontFactory.getFont(FontFactory.COURIER, 12)));
+//				relatorioPDF.add(new Paragraph("Total Arrecadado no Crédito: R$ " + relatorio.get(1),
+//						FontFactory.getFont(FontFactory.COURIER, 12)));
+//				relatorioPDF.add(new Paragraph("Total Arrecadado no Débito: R$ " + relatorio.get(2),
+//						FontFactory.getFont(FontFactory.COURIER, 12)));
+//				relatorioPDF.add(new Paragraph("Total Arrecadado À Vista: R$ " + relatorio.get(3),
+//						FontFactory.getFont(FontFactory.COURIER, 12)));
+//
+//			} catch (DocumentException de) {
+//				System.err.println(de.getMessage());
+//			} catch (IOException ioe) {
+//				System.err.println(ioe.getMessage());
+//			} finally {
+//				relatorioPDF.close();
+//			}
+//			return MODAL_DETALHAMENTO_MENSAL;
+//
+//		} else {
+//			model.addAttribute("msgSemRelatorio", true);
+//			return PAGINA_DETALHAMENTO_FINANCEIRO;
+//		}
+//	}
 }
