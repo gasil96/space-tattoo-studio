43a71725e004741bc3576efb35534af14b402139
Gabriel Silva <gasil96@gmail.com>
2020-02-17 11:42:34 -0300

    reponsividade tabelas
    Signed-off-by: Gabriel Silva <gasil96@gmail.com>

--- src/main/java/br/com/gbsoftware/spacetattoostudio/controller/FinanceiroController.java
+++ src/main/java/br/com/gbsoftware/spacetattoostudio/controller/FinanceiroController.java
@@ -1,11 +1,8 @@
 package br.com.gbsoftware.spacetattoostudio.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import br.com.gbsoftware.spacetattoostudio.domain.model.Cliente;
 
 @Controller
 @RequestMapping("financeiro")
@@ -14,7 +11,7 @@ public class FinanceiroController {
 	private static final String PAGINA_DETALHAMENTO_FINANCEIRO = "detalhamento/financeiro-detalhado";
 
 	@GetMapping("detalhamento")
-	public String detalhamentoFinanceiro(Model model, Cliente cliente) {
+	public String detalhamentoFinanceiro() {
 
 		return PAGINA_DETALHAMENTO_FINANCEIRO;
 	}
