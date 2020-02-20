18f3f972de8683429800604b1a78d2f957dcea38
Gabriel Silva <gasil96@gmail.com>
2020-02-20 14:37:18 -0300

    wpp enviar msg via ofertas impl
    Signed-off-by: Gabriel Silva <gasil96@gmail.com>

--- src/main/java/br/com/gbsoftware/spacetattoostudio/controller/PromocionalController.java
+++ src/main/java/br/com/gbsoftware/spacetattoostudio/controller/PromocionalController.java
@@ -1,26 +1,19 @@
 package br.com.gbsoftware.spacetattoostudio.controller;
 
 import java.math.BigDecimal;
-import java.net.URI;
-import java.net.URISyntaxException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.view.RedirectView;
 
 import br.com.gbsoftware.spacetattoostudio.domain.model.Cliente;
 import br.com.gbsoftware.spacetattoostudio.domain.model.CorpoWhatsApp;
@@ -37,8 +30,8 @@ public class PromocionalController {
 	private static final String PAGINANA_DETALHAMENTO_PROMOCIONAL = "detalhamento/promocional-detalhado";
 	private static final String ATT_PAGINA = "redirect:detalhamento";
 	private static final String MODAL_ENVIAR_MSG_WPP = "modal/modal-enviar-msg-wpp";
-	private static final String WHATSAPP_URL = "http://www.wa.me/55";
-	
+	private static final String WHATSAPP_URL = "wa.me/55";
+
 	@Autowired
 	private ClienteService servicoCliente;
 
@@ -47,21 +40,21 @@ public class PromocionalController {
 
 	@Autowired
 	private VwClienteDadosPiercingService servicoVwClientePiercing;
-	
+
 	@GetMapping("detalhamento")
 	public String detalhamentoPromocional(Cliente cliente, Model model) {
 		List<VwClienteDadosTattoo> listaVwTattoo = servicoVwClienteTattoo.buscarTodos();
 		List<VwClienteDadosPiercing> listaVwPiercing = servicoVwClientePiercing.buscarTodos();
-		
+
 		model.addAttribute("topTattoo",
 				listaVwTattoo.stream()
 						.sorted(Comparator.comparingLong(VwClienteDadosTattoo::getNumeroAgendamentos).reversed())
 						.limit(10).collect(Collectors.toList()));
-		
+
 		model.addAttribute("topPiercing",
 				listaVwPiercing.stream()
-				.sorted(Comparator.comparingLong(VwClienteDadosPiercing::getNumeroAgendamentos).reversed())
-				.limit(10).collect(Collectors.toList()));
+						.sorted(Comparator.comparingLong(VwClienteDadosPiercing::getNumeroAgendamentos).reversed())
+						.limit(10).collect(Collectors.toList()));
 
 		return PAGINANA_DETALHAMENTO_PROMOCIONAL;
 	}
@@ -83,38 +76,19 @@ public class PromocionalController {
 	}
 
 	@GetMapping("whatsapp/{id}")
-	public String msgWhatsApp(@PathVariable("id")Long id, Model model, CorpoWhatsApp corpoWpp) throws Exception {
-			Cliente clienteLocalizado = servicoCliente.buscarPorId(id).orElse(null);
-			if(clienteLocalizado == null) {
-				 throw new Exception("Cliente com id: "+id+" não existe!");
-			}else {
-				// TODO ADICIONAR VARIAVEIS DO CLIENTE SELECIONADO E MELHORAR MENSSAGEM
-				corpoWpp.setMsg("Olá 'Nome Fulano' você está na lista dos tops da Space Tatto Studio e "
-							+ "com isso acaba de ganhar uma oferta especial para 'TipoServico' responda esssa menssagem para mais informações, Oferta válida até 'Data'");
-				corpoWpp.setNumero(clienteLocalizado.getTelefone());
-				model.addAttribute("whatsapp", corpoWpp);
-				return MODAL_ENVIAR_MSG_WPP;
-			}
+	public String msgWhatsApp(@PathVariable("id") Long id, Model model, CorpoWhatsApp corpoWpp) throws Exception {
+		Cliente clienteLocalizado = servicoCliente.buscarPorId(id).orElse(null);
+		if (clienteLocalizado == null) {
+			throw new Exception("Cliente com id: " + id + " não existe!");
+		} else {
+			corpoWpp.setMsg(msgOferta(clienteLocalizado));
+			corpoWpp.setNumero(clienteLocalizado.getTelefone());
+			model.addAttribute("url", urlWhatsApp(corpoWpp));
+			model.addAttribute("whatsapp", corpoWpp);
+			return MODAL_ENVIAR_MSG_WPP;
+		}
 	}
-	
-	@RequestMapping("enviar-msg-wpp")
-	public RedirectView enviarWhats(CorpoWhatsApp corpoWpp, HttpServletResponse response) {
-//		System.err.println("Link: ---------------------------->"+urlWhatsApp(corpoWpp));
-		RedirectView redirectView = new RedirectView();
-		redirectView.setUrl(urlWhatsApp(corpoWpp));
-		return redirectView;
-	}
-	
-	@RequestMapping(value = "/to-be-redirected", method = RequestMethod.GET)
-	public ResponseEntity<Object> redirectToExternalUrl(CorpoWhatsApp corpoWpp) throws URISyntaxException {
-		String teste = urlWhatsApp(corpoWpp);
-		System.err.println(teste);
-		URI yahoo = new URI("http://localhost:8085/promocao/detalhamento");
-	    HttpHeaders httpHeaders = new HttpHeaders();
-	    httpHeaders.setLocation(yahoo);
-	    return new ResponseEntity<>(httpHeaders, HttpStatus.SEE_OTHER);
-	}
-	
+
 	@PostMapping("consumir-credito")
 	public String removerCredito(Cliente cliente) throws Exception {
 		Cliente clienteLocalizado = servicoCliente.buscarPorId(cliente.getId()).orElse(null);
@@ -131,12 +105,22 @@ public class PromocionalController {
 
 		return ATT_PAGINA;
 	}
-	
+
 	private String urlWhatsApp(CorpoWhatsApp corpoWpp) {
 		String numeroTratato = corpoWpp.getNumero().replaceAll("\\D", "");
 		corpoWpp.setNumero(numeroTratato);
-		String urlParametriza = WHATSAPP_URL+numeroTratato+"?text="+corpoWpp.getMsg();
+		String urlParametriza = WHATSAPP_URL + numeroTratato + "?text=" + corpoWpp.getMsg();
 		return urlParametriza;
 	}
-	
+
+	private String msgOferta(Cliente cliente) {
+		DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		String menssagem = "Olá " + cliente.getNome()
+				+ ", você está na lista dos *MELHORES CLIENTES* do Space Tattoo Studio e "
+				+ "com isso acaba de ganhar uma oferta especial para você. Responda esssa mensagem para mais informações "
+				+ " *Oferta válida até:* "
+				+ LocalDateTime.now().plusDays(15).format(dateFormat);
+		return menssagem;
+	}
+
 }
