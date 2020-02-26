34720c40eeb1f2eee66b9005ddcf1df93a4c3e44
Gabriel Silva <gasil96@gmail.com>
2020-02-26 13:28:54 -0300

    impl smtp gmail
    Signed-off-by: Gabriel Silva <gasil96@gmail.com>

--- src/main/java/br/com/gbsoftware/spacetattoostudio/SpacetattooApplication.java
+++ src/main/java/br/com/gbsoftware/spacetattoostudio/SpacetattooApplication.java
@@ -1,7 +1,5 @@
 package br.com.gbsoftware.spacetattoostudio;
 
-import java.util.Properties;
-
 import org.springframework.beans.factory.annotation.Value;
 /**
  * <b>Gabriel S. Sofware</b>
@@ -12,11 +10,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.EventListener;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 @Configuration
 @SpringBootApplication
@@ -34,22 +29,6 @@ public class SpacetattooApplication {
 		System.out.println("############## " + xpto);
 	}
 
-	@Bean
-	public JavaMailSender getJavaMailSender() {
-		JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-		mailSender.setHost("smtp.gmail.com");
-		mailSender.setPort(587);
-
-		mailSender.setUsername("gasil96@gmail.com");
-		mailSender.setPassword("#euamo694314");
-
-		Properties props = mailSender.getJavaMailProperties();
-		props.put("mail.transport.protocol", "smtp");
-		props.put("mail.smtp.auth", "true");
-		props.put("mail.smtp.starttls.enable", "true");
-		props.put("mail.debug", "true");
-
-		return mailSender;
-	}
+	
 
 }
---
+++ src/main/java/br/com/gbsoftware/spacetattoostudio/config/EmailConfiguration.java
@@ -0,0 +1,38 @@
+package br.com.gbsoftware.spacetattoostudio.config;
+
+import java.util.Properties;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+@Configuration
+public class EmailConfiguration {
+
+	@Value("${USERNAMER_EMAIL}")
+	public String username;
+	
+	@Value("${PASSWORD_EMAIL}")
+	public String password;
+	
+	@Bean
+	public JavaMailSender getJavaMailSender() {
+		JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+		mailSender.setHost("smtp.gmail.com");
+		mailSender.setPort(587);
+
+		mailSender.setUsername(username);
+		mailSender.setPassword(password);
+
+		Properties props = mailSender.getJavaMailProperties();
+		props.put("mail.transport.protocol", "smtp");
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.debug", "true");
+
+		return mailSender;
+	}
+	
+}
