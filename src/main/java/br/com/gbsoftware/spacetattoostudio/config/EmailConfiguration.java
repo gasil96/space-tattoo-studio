9185dfc43454f4f5ea5e06e6a76131097947a0d7
Gabriel Silva <gasil96@gmail.com>
2020-02-26 13:39:20 -0300

    bug user name
    Signed-off-by: Gabriel Silva <gasil96@gmail.com>

--- src/main/java/br/com/gbsoftware/spacetattoostudio/config/EmailConfiguration.java
+++ src/main/java/br/com/gbsoftware/spacetattoostudio/config/EmailConfiguration.java
@@ -11,7 +11,7 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
 @Configuration
 public class EmailConfiguration {
 
-	@Value("${USERNAMER_EMAIL}")
+	@Value("${USERNAME_EMAIL}")
 	public String username;
 	
 	@Value("${PASSWORD_EMAIL}")
